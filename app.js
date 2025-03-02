a4512fbaeca2c87a52f66173ac7821c69bbe9f2c
referentetic <gabrielpcelectronica@gmail.com>
2025-03-02 04:51:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+// Lógica de la suma de los números
 document.getElementById('sumarBtn').addEventListener('click', function() {
     let num1 = parseFloat(document.getElementById('num1').value);
     let num2 = parseFloat(document.getElementById('num2').value);
     let resultado = num1 + num2;
     document.getElementById('resultado').textContent = `Resultado: ${resultado}`;
 });
+
 // Registrar el Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js') // Asegúrate de que el archivo 'service-worker.js' esté en la raíz
             .then((registration) => {
                 console.log('Service Worker registrado con éxito:', registration);
             })
